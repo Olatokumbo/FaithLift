b853a0110910475eb23d195f94359eec41fc75a4
David Odesola <faithodesola@gmail.com>
2021-02-12 14:32:30 +0100

    Working on the Poster Lists

--- src/pages/Movies/Movies.js
+++ src/pages/Movies/Movies.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Poster from "../../assets/images/poster.png";
+import Movie from "../../assets/images/jailer.jpg";
 import { Typography, Button } from "@material-ui/core";
 import style from "./Movies.module.css";
 
@@ -21,6 +22,63 @@ const Movies = () => {
         </div>
         <img src={Poster} alt="poster" />
         <div className={style.fadeBottom}></div>
+      </div>
+      <div className={style.movieListContainer}>
+        <div className={style.movieList}>
+          <Typography className={style.listHeader}>LATEST FILMS</Typography>
+          <div className={style.posterList}>
+            <img className={style.poster} src={Movie} alt="movie" />
+            <img className={style.poster} src={Movie} alt="movie" />
+            <img className={style.poster} src={Movie} alt="movie" />
+            <img className={style.poster} src={Movie} alt="movie" />
+            <img className={style.poster} src={Movie} alt="movie" />
+            <img className={style.poster} src={Movie} alt="movie" />
+            <img className={style.poster} src={Movie} alt="movie" />
+            <img className={style.poster} src={Movie} alt="movie" />
+            <img className={style.poster} src={Movie} alt="movie" />
+            <img className={style.poster} src={Movie} alt="movie" />
+          </div>
+        </div>
+        <div className={style.movieList}>
+          <Typography className={style.listHeader}>LATEST FILMS</Typography>
+          <div className={style.posterList}>
+            <img className={style.poster} src={Movie} alt="movie" />
+            <img className={style.poster} src={Movie} alt="movie" />
+            <img className={style.poster} src={Movie} alt="movie" />
+            <img className={style.poster} src={Movie} alt="movie" />
+            <img className={style.poster} src={Movie} alt="movie" />
+            <img className={style.poster} src={Movie} alt="movie" />
+            <img className={style.poster} src={Movie} alt="movie" />
+            <img className={style.poster} src={Movie} alt="movie" />
+            <img className={style.poster} src={Movie} alt="movie" />
+            <img className={style.poster} src={Movie} alt="movie" />
+            <img className={style.poster} src={Movie} alt="movie" />
+            <img className={style.poster} src={Movie} alt="movie" />
+            <img className={style.poster} src={Movie} alt="movie" />
+            <img className={style.poster} src={Movie} alt="movie" />
+            <img className={style.poster} src={Movie} alt="movie" />
+            <img className={style.poster} src={Movie} alt="movie" />
+            <img className={style.poster} src={Movie} alt="movie" />
+            <img className={style.poster} src={Movie} alt="movie" />
+            <img className={style.poster} src={Movie} alt="movie" />
+            <img className={style.poster} src={Movie} alt="movie" />
+          </div>
+        </div>
+        <div className={style.movieList}>
+          <Typography className={style.listHeader}>LATEST FILMS</Typography>
+          <div className={style.posterList}>
+            <img className={style.poster} src={Movie} alt="movie" />
+            <img className={style.poster} src={Movie} alt="movie" />
+            <img className={style.poster} src={Movie} alt="movie" />
+            <img className={style.poster} src={Movie} alt="movie" />
+            <img className={style.poster} src={Movie} alt="movie" />
+            <img className={style.poster} src={Movie} alt="movie" />
+            <img className={style.poster} src={Movie} alt="movie" />
+            <img className={style.poster} src={Movie} alt="movie" />
+            <img className={style.poster} src={Movie} alt="movie" />
+            <img className={style.poster} src={Movie} alt="movie" />
+          </div>
+        </div>
       </div>
     </div>
   );
