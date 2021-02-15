313a508b0aae6f9e69a9ff14841dfe5a575562a7
David Odesola <faithodesola@gmail.com>
2021-02-15 12:34:20 +0100

    Adding ScrollToTop function

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import Footer from "./components/Footer/Footer";
 import Home from "./pages/Home/Home";
 import Movies from "./pages/Movies/Movies";
 import Articles from "./pages/Articles/Articles";
+import ScrollToTop from "./ScrollToTop";
 import "./App.css";
 const App = () => {
   return (
     <Router>
       <Navbar />
+      <ScrollToTop/>
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/movies" component={Movies} />
